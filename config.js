8cf44321bcba681c3e5476afb65d4644b3704704
boybenjx7 <josebenjamin202444@gmail.com>
2024-05-12 14:41:11 +1300

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,33 @@
 /* ⚙️ Credits to:
-* AzamiJs
-* Elrebelde21
-* GataNina-Li */
+* @boybenjx7
+* Hyper´V
+* Hyper´V */
 
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath, pathToFileURL } from 'url'
 
-global.owner = [['524531287294', 'CuriosityBot-MD 🍧', true], ['447700168473', '👑 𝐎𝐰𝐧𝐞𝐫 👑', true], ['5217294888993'], ['5214531287294'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['59162714296'], ['5492964650915'], ['50558124470'], ['5214434703586'], ['18293132203'], ['573003025991']]
+global.owner = [['𝑯𝒚𝒑𝒆𝒓´V', '𝑯𝒚𝒑𝒆𝒓´V', true], ['51936732723', ' 𝐎𝐰𝐧𝐞𝐫 ', true], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723']]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = '' //Ejemplo: +59309090909
+global.botNumberCode = '' //Ejemplo: +51936732723
 global.confirmCode = ''
 
-global.suittag = ['5214531287294']
+global.suittag = ['51936732723']
 global.mods = []
 global.prems = []
 
 //Solo desarrolladores aprobados
-global.isdev = [ /* ['5217294888993'], */ ['5214434703586'], ['5492266466080'], ['5492266613038'], ['573012482597'], ['5215610314499'], ['51935531943'], ['33760536110'], ['5492964650915'], ['50558124470']]
+global.isdev = [ /* ['51936732723'], */ ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723'], ['51936732723']]
 
 //Aqui encuentras los nombres del bot 
 global.packname = ''
-global.author = 'CuriosityBot'
-global.wm = '© CuriosityBot-MD'
-global.wm2 = 'Curiosity : Bot'
-global.azami = 'Azami'
-global.cb = 'CuriosityBot'
+global.author = '𝑯𝒚𝒑𝒆𝒓´V'
+global.wm = '𝑯𝒚𝒑𝒆𝒓´V'
+global.wm2 = '𝑯𝒚𝒑𝒆𝒓´V : Bot'
+global.azami = '@boybenjx7'
+global.cb = '𝑯𝒚𝒑𝒆𝒓´V'
 
 
 //Las Versiones Y Entre Otros
