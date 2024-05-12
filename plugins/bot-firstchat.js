6f029a509825124e8fb8a0d98f048cf05230a222
boybenjx7 <josebenjamin202444@gmail.com>
2024-05-13 03:51:55 +1300

    Update bot-firstchat.js

--- plugins/bot-firstchat.js
+++ plugins/bot-firstchat.js
@@ -7,17 +7,17 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 86400000) return
-await conn.reply(m.chat, `👋 Hola ${nombre}!!
+await conn.reply(m.chat, `👋 Hola 𝑯𝒚𝒑𝒆𝒓´V ${nombre}!!
  *${saludo}*
 
 📅 Fecha: ${fecha}
 ⏰ Hora: ${tiempo}
 
-⚠️ *Nota:* no envíe spam al bot
-🧃 Escriba *.menu* para mostrar el menú 
+⚠️ *Nota:* no envíe spam al bot 𝑯𝒚𝒑𝒆𝒓´V
+🧃 Escriba *.menu* para mostrar el menú 𝑯𝒚𝒑𝒆𝒓´V
   
 📝 ¿Quieres apoyar este proyecto para que siga actualizándose? únete a nuestro grupo: 
-*https://chat.whatsapp.com/EfWlxyVXlZ7AWVJ6sjJvyI*`, m, fake, )
+*https://chat.whatsapp.com/DXAdja6QqGsEqMtuTJNTXn*`, m, fake, )
 
 
 user.pc = new Date * 1
